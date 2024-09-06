91490256f6718903c1062c559247de10b32434a2
ma.kurhanovich <ma.kurhanovich@vebtech.by>
2024-09-06 14:48:44 +0300

    Added base cipher selection

--- src/main/kotlin/ui/MainScreen.kt
+++ src/main/kotlin/ui/MainScreen.kt
@@ -1,20 +1,23 @@
 package ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import dev.bababnanick.crypto_decoding.generated.resources.Res
+import dev.bababnanick.crypto_decoding.generated.resources.ciphers
 import dev.bababnanick.crypto_decoding.generated.resources.encrypt
 import dev.bababnanick.crypto_decoding.generated.resources.welcome
+import org.jetbrains.compose.resources.stringArrayResource
 import org.jetbrains.compose.resources.stringResource
 import ui.components.CipherOutlinedTextField
 import ui.components.TextFieldLabel
@@ -43,13 +46,13 @@ class MainScreen : Screen {
         Row(
             modifier = modifier.fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             CipherMethodSelection(
-                modifier = Modifier.fillMaxWidth(0.5f)
+                modifier = Modifier.fillMaxWidth(0.4f)
             )
             WorkingArea(
-                modifier = Modifier.fillMaxWidth(0.5f),
+                modifier = Modifier.fillMaxWidth(0.6f),
                 model = model,
                 state = state
             )
@@ -63,7 +66,34 @@ class MainScreen : Screen {
         Column(
             modifier = modifier
         ) {
+            val options = stringArrayResource(Res.array.ciphers)
+            val (selectedOption, onOptionSelected) = remember { mutableStateOf(options.first() ) }
 
+            options.forEach { cipher ->
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(CipherTheme.dimensions.smallDefault))
+                        .selectable(
+                            selected = (cipher == selectedOption),
+                            onClick = {
+                                onOptionSelected(cipher)
+                            }
+                        )
+                        .padding(horizontal = CipherTheme.dimensions.smallDefault),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = (cipher == selectedOption),
+                        onClick = { onOptionSelected(cipher) }
+                    )
+                    Text(
+                        text = cipher,
+                        style = CipherTheme.typography.bold.merge(),
+                        modifier = Modifier.padding(start = CipherTheme.dimensions.smallDefault)
+                    )
+                }
+            }
         }
     }
 
--- src/main/kotlin/ui/config/CompositionsLocal.kt
+++ src/main/kotlin/ui/config/CompositionsLocal.kt
@@ -5,3 +5,7 @@ import androidx.compose.runtime.staticCompositionLocalOf
 val LocalCipherDimensions = staticCompositionLocalOf<CipherDimensions> {
     error("No CipherDimensions provided")
 }
+
+val LocalCipherTypography = staticCompositionLocalOf<CipherTypography> {
+    error("No CipherTypography provided")
+}
